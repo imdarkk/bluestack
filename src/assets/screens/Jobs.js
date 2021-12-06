751d4d8a55f997873d7696461d0c0e0790fd6a8c
ImDarkk <mkyriakou52@st.schools.ac.cy>
2021-12-06 08:18:22 +0200

    custom new event screen

--- src/assets/screens/Jobs.js
+++ src/assets/screens/Jobs.js
@@ -11,6 +11,8 @@ import {
 	ViewDirective,
 } from "@syncfusion/ej2-react-schedule";
 import { DataManager, UrlAdaptor } from "@syncfusion/ej2-data";
+import { DropDownListComponent } from '@syncfusion/ej2-react-dropdowns';
+import { DateTimePickerComponent } from '@syncfusion/ej2-react-calendars';
 import Navigation from "../components/Navigation";
 import HamburgerButton from "../components/HamburgerButton";
 import { useAuth } from "../auth/auth-context";
@@ -27,10 +29,24 @@ const Jobs = () => {
 		adaptor: new UrlAdaptor(),
 	});
 
+	const templateCalendar = () => {
+		return (
+			// Add code to style custom calendar
+			<div>
+				<input id="CustomerName" name="CustomerName" class="e-field e-input" type="text" placeholder="Customer Name" />
+				<input id="Address" name="Address" class="e-field e-input" type="text" placeholder="Address" />
+				<input id="Phone" name="Phone" class="e-field e-input" type="text" placeholder="Phone #" />
+				<DateTimePickerComponent id="Time" format='dd/MM/yy hh:mm a' data-name="Time" value={new Date()} className="e-field"></DateTimePickerComponent>
+				<DropDownListComponent id="Status" placeholder='Choose status' data-name='Status' className="e-field" style={{ width: '100%' }}
+					dataSource={['Active', 'Pending', 'Closed']}></DropDownListComponent>
+			</div>
+		)
+	}
+
 	return (
 		<Container bg="#10151A" maxW="100vw" color="white">
 			<HamburgerButton />
-			<ScheduleComponent eventSettings={{ dataSource: dataManager }}>
+			<ScheduleComponent eventSettings={{ dataSource: dataManager }} editorTemplate={templateCalendar}>
 				<ViewsDirective>
 					<ViewDirective
 						option="Week"
